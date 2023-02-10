e1d3cf558e5f1855e7d3ee4200c275c8d5cc8bf4
Chall Fry <chall.fry@gmail.com>
2023-02-09 22:11:58 -0800

    Catalyst doesn't seem to activate audio sessions.

--- Kraken/Phone/PhonecallDataManager.swift
+++ Kraken/Phone/PhonecallDataManager.swift
@@ -801,12 +801,20 @@ extension PhonecallDataManager: CXProviderDelegate {
 	
     func provider(_ provider: CXProvider, perform action: CXStartCallAction) {
 		logger.info("Got told to start a call.")
+		#if targetEnvironment(macCatalyst)
+			configureAudioSession(audioSession: audioSession)
+    		startAudio(audioSession: audioSession)
+		#endif
 		action.fulfill()
     }
 
     func provider(_ provider: CXProvider, perform action: CXAnswerCallAction) {
 		logger.info("Got told CXAnswerCallAction.")
 		openAnswerSocket()
+		#if targetEnvironment(macCatalyst)
+			configureAudioSession(audioSession: audioSession)
+    		startAudio(audioSession: audioSession)
+		#endif
 		action.fulfill()
     }
     
@@ -814,6 +822,10 @@ extension PhonecallDataManager: CXProviderDelegate {
     func provider(_ provider: CXProvider, perform action: CXEndCallAction) {
 		logger.info("Got told CXEndCallAction.")
 		endCall()
+		#if targetEnvironment(macCatalyst)
+			stopAudio()
+			try? session.setActive(false)	
+		#endif
 		action.fulfill()
     }
     
